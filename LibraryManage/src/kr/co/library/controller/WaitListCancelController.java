3309c4b697cc7663cc0428bdd0a9147053a2b264
Kim Jinkwang <kwang0422@naver.com>
2017-05-23 16:01:57 +0900

    LibraryManage
    RentBookController 생성 (대출신청)
    WaitListInsertController 생성(대기자 신청)
    WaitListInsertController 생성(대기취소)
    
    RentalServiceImpl 수정 - rentBook() 기능 분할 수정.
    
    web.xml - servlet 등록.

---
+++ LibraryManage/src/kr/co/library/controller/WaitListCancelController.java
@@ -0,0 +1,27 @@
+package kr.co.library.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import kr.co.library.service.RentalService;
+import kr.co.library.service.impl.RentalServiceImpl;
+
+public class WaitListCancelController extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session = req.getSession();
+		session.setAttribute("userId", "userId");
+		session.setAttribute("bookId", "bookId");
+		
+		RentalService service = RentalServiceImpl.getInstance();
+		service.cancelWaitBook((String)(session.getAttribute("userId")), (String)(session.getAttribute("bookId")));
+		
+		resp.sendRedirect("/LibraryManage/rental/test.jsp");
+	}
+}
