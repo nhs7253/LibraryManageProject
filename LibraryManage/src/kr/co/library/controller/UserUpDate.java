0c403e88b068a4139c9b9b96e642b5586539b44e
yonghoon <hiz7777@naver.com>
2017-05-23 18:23:17 +0900

    LibraryManage
    UserUpDate
    
    xml 추가

---
+++ LibraryManage/src/kr/co/library/controller/UserUpDate.java
@@ -0,0 +1,23 @@
+package kr.co.library.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class UserUpDate extends HttpServlet
+{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
+	{
+		
+		
+		
+	}
+		
+	
+	
+}
